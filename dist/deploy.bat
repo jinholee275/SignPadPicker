e1cfc08c9eaf8918317869c68f5b907bde614612
Jinho Lee <ruinbible@ezcaretech.com>
2023-08-25 13:18:48 +0900

    배포 작업 배치 생성

---
+++ dist/deploy.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+xcopy /q /y /s ..\CHANGELOG SignPadPicker-v%1\CHANGELOG\
+xcopy /q /y /s ..\bin\x86\Release\SignPadPicker.dll SignPadPicker-v%1\publish\SignPadPicker\
+xcopy /q /y /s ..\bin\x86\Release\SignPadPicker.ScreenSignPadAdaptor.dll SignPadPicker-v%1\publish\SignPadPicker.ScreenSignPadAdaptor\
+
+xcopy /q /y /s ..\bin\x86\Release\SignPadPicker.KisSignPadAdaptor.dll SignPadPicker-v%1\publish\SignPadPicker.KisSignPadAdaptor\
+xcopy /q /y /s ..\bin\x86\Release\KisDongleDll.dll SignPadPicker-v%1\publish\SignPadPicker.KisSignPadAdaptor\
+
+xcopy /q /y /s ..\bin\x86\Release\SignPadPicker.KscatSignPadAdaptor.dll SignPadPicker-v%1\publish\SignPadPicker.KscatSignPadAdaptor\
+xcopy /q /y /s ..\bin\x86\Release\ksnetcomm.dll SignPadPicker-v%1\publish\SignPadPicker.KscatSignPadAdaptor\
+
+xcopy /q /y /s ..\bin\x86\Release\SignPadPicker.NicePosSignPadAdaptor.dll SignPadPicker-v%1\publish\SignPadPicker.NicePosSignPadAdaptor\
+xcopy /q /y /s ..\bin\x86\Release\NICEPOSICV105.dll SignPadPicker-v%1\publish\SignPadPicker.NicePosSignPadAdaptor\
+
+xcopy /q /y /s ..\bin\x86\Release\SignPadPicker.SmartroSignPadAdaptor.dll SignPadPicker-v%1\publish\SignPadPicker.SmartroSignPadAdaptor\
+xcopy /q /y /s ..\bin\x86\Release\libeay32_smt.dll SignPadPicker-v%1\publish\SignPadPicker.SmartroSignPadAdaptor\
+xcopy /q /y /s ..\bin\x86\Release\ssleay32_smt.dll SignPadPicker-v%1\publish\SignPadPicker.SmartroSignPadAdaptor\
+xcopy /q /y /s ..\bin\x86\Release\SmartroSign.dll SignPadPicker-v%1\publish\SignPadPicker.SmartroSignPadAdaptor\
+
+zip -9vr SignPadPicker-v%1.zip ./SignPadPicker-v%1/
